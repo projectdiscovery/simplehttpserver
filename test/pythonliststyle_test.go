e33a7bd0ed0830c066d4b7e857e30304668d721e
li6in9muyou <libq5520@mails.jlu.edu.cn>
2023-03-15 15:39:05 +0800

    reword

--- test/pythonliststyle_test.go
+++ test/pythonliststyle_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 )
 
-func TestWritePythonStyleHtmlPageForDirectories(t *testing.T) {
+func TestServePythonStyleHtmlPageForDirectories(t *testing.T) {
 	const want = `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
