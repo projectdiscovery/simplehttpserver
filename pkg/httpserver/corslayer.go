3a7bc37ce3589810d90fa3348a509760bdab5d14
shubhamrasal <shubhamdharmarasal@gmail.com>
2023-03-15 14:13:46 +0530

    Update header add location

--- pkg/httpserver/corslayer.go
+++ pkg/httpserver/corslayer.go
@@ -7,18 +7,16 @@ import (
 
 func (t *HTTPServer) corslayer(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		headers := w.Header()
+		headers.Set("Access-Control-Allow-Origin", "*")
 		if r.Method != http.MethodOptions {
 			handler.ServeHTTP(w, r)
 			return
 		}
 
-		headers := w.Header()
-
 		headers.Add("Vary", "Origin")
 		headers.Add("Vary", "Access-Control-Request-Method")
 		headers.Add("Vary", "Access-Control-Request-Headers")
-
-		headers.Set("Access-Control-Allow-Origin", "*")
 
 		reqMethod := r.Header.Get("Access-Control-Request-Method")
 		if reqMethod != "" {
